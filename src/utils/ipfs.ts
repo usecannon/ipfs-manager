26589292270a2ace2bc08c22cf3657603e8d396e
Matías <mjlescano@protonmail.com>
2023-03-30 14:59:01 +0300

    add auth option

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,19 +1,26 @@
 import FormData from "form-data";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { Buffer } from "buffer";
 
 export const ipfsUpload = async (ipfsUrl: string, content: string) => {
   if (!content) throw new Error("No content to upload");
+
+  const url = new URL("/api/v0/add", ipfsUrl);
+  const opts: AxiosRequestConfig = {};
+
+  if (url.username) {
+    opts.auth = {
+      username: url.username,
+      password: url.password,
+    };
+  }
 
   const formData = new FormData();
   const buff = Buffer.from(content);
 
   formData.append("data", Buffer.from(buff));
 
-  const result = await axios.post(
-    ipfsUrl.replace("+ipfs", "") + "/api/v0/add",
-    formData
-  );
+  const result = await axios.post(url.toString(), formData, opts);
 
   console.log("uploaded", result.statusText, result.data.Hash);
 
