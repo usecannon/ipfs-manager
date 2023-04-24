0a85eed1473d6dfe4eb20d581fd539030134c07d
Matías <mjlescano@protonmail.com>
2023-04-24 14:31:18 +0200

    update font

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,18 @@
-// 1. import `extendTheme` function
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import type { ThemeConfig } from '@chakra-ui/react'
 
-// 2. Add your color mode config
+import '@fontsource/nunito-sans/400.css'
+import '@fontsource/nunito-sans/700.css'
+
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 } satisfies ThemeConfig
 
-export const theme = extendTheme({ config })
+export const theme = extendTheme({
+  config,
+  fonts: {
+    heading: `'Nunito Sans', sans-serif`,
+    body: `'Nunito Sans', sans-serif`,
+  },
+})
